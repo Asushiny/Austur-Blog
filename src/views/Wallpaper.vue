d44ac9fd4b5bed9c767bb4a919ed296d20cab7d5
Asushiny <1204693790@qq.com>
2025-01-04 21:59:23 +0800

    blog

--- src/views/Wallpaper.vue
+++ src/views/Wallpaper.vue
@@ -9,8 +9,15 @@
 	} from 'vue-waterfall-plugin-next'
 	import 'vue-waterfall-plugin-next/dist/style.css'
 	import {
+		ArrowLeftBold,
+		ArrowRightBold
+	} from '@element-plus/icons-vue'
+	import {
 		getImage
 	} from '@/api/index.js'
+	import {
+		ElMessage
+	} from 'element-plus'
 
 	const imageslist = ref([])
 
@@ -28,12 +35,51 @@
 	const showImg = (val) => {
 		showImagePreview.value = true
 		showUrl.value = [val.target.currentSrc]
-		console.log(showUrl.value)
 	}
 	//预览关闭
 	const closePreview = () => {
 		showUrl.value = []
 		showImagePreview.value = false
+	}
+	// 预览上一页
+	const showLeft = (val) => {
+		var midChar = showUrl.value[0].charAt(showUrl.value[0].length - 6);
+		//判断倒数第六位是否为/
+		if (midChar === '/') {
+			const newChar = String(Number(showUrl.value[0].slice(-5, -4)) - 1);
+			if (newChar === '0') {
+				ElMessage({
+					message: '已经是第一张啦！',
+					grouping: true,
+					type: 'warning',
+				})
+			} else {
+				showUrl.value = [showUrl.value[0].slice(0, -5) + newChar + showUrl.value[0].slice(-4)]
+			}
+		} else {
+			const newChar = String(Number(showUrl.value[0].slice(-6, -4)) - 1);
+			showUrl.value = [showUrl.value[0].slice(0, -6) + newChar + showUrl.value[0].slice(-4)]
+		}
+	}
+	// 预览下一页
+	const showRight = (val) => {
+		var midChar = showUrl.value[0].charAt(showUrl.value[0].length - 6);
+		//判断倒数第六位是否为/
+		if (midChar === '/') {
+			const newChar = String(Number(showUrl.value[0].slice(-5, -4)) + 1);
+			showUrl.value = [showUrl.value[0].slice(0, -5) + newChar + showUrl.value[0].slice(-4)]
+		} else {
+			const newChar = String(Number(showUrl.value[0].slice(-6, -4)) + 1);
+			if (newChar == imageslist.value.length + 1) {
+				ElMessage({
+					message: '已经到最后一张啦！',
+					grouping: true,
+					type: 'warning',
+				})
+			} else {
+				showUrl.value = [showUrl.value[0].slice(0, -6) + newChar + showUrl.value[0].slice(-4)]
+			}
+		}
 	}
 </script>
 
@@ -45,10 +91,37 @@
 			</div>
 		</template>
 	</Waterfall>
+
 	<!-- 图片预览 -->
-	<el-image-viewer v-if="showImagePreview" @close="closePreview" :url-list="showUrl" />
+	<div v-if="showImagePreview">
+		<el-image-viewer @close="closePreview" :url-list="showUrl" />
+		<el-button class="b-left" @click="showLeft" :icon="ArrowLeftBold" size="large" color="#606266" circle />
+		<el-button class="b-right" @click="showRight" :icon="ArrowRightBold" size="large" color="#606266" circle />
+	</div>
 </template>
 
 <style scoped>
-	
+	.card {
+		cursor: pointer;
+	}
+
+	.b-left {
+		opacity: 0.7;
+		position: absolute;
+		left: 5%;
+		top: 45%;
+		z-index: 9999;
+		background-color: #606266;
+		border-color: #606266;
+	}
+
+	.b-right {
+		opacity: 0.7;
+		position: absolute;
+		right: 5%;
+		top: 45%;
+		z-index: 9999;
+		background-color: #606266;
+		border-color: #606266;
+	}
 </style>
